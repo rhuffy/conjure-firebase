7d7aa791c9db733e208e361ac8a55103e98be955
Raymond Huffman <rhuffman@mit.edu>
2020-09-17 15:24:03 -0400

    cleanup scripts

--- scripts/install-conjure.sh
+++ scripts/install-conjure.sh
@@ -1,8 +1,8 @@
 #!/bin/sh
 CONJURE_VERSION=$1
 if [ -d "conjure-${CONJURE_VERSION}" ]; then
-  echo "Conjure ${CONJURE_VERSION} already installed"
+  echo "Conjure ${CONJURE_VERSION} already installed\n"
 else
-  echo "Installing Conjure ${CONJURE_VERSION}"
+  echo "Installing Conjure ${CONJURE_VERSION}\n"
   curl -sL https://palantir.bintray.com/releases/com/palantir/conjure/conjure/${CONJURE_VERSION}/conjure-${CONJURE_VERSION}.tgz | tar zx
 fi
--- scripts/run-conjure.sh
+++ scripts/run-conjure.sh
@@ -2,4 +2,5 @@
 CONJURE_VERSION=$1
 rm -rf conjure-api && mkdir conjure-api &&
 ./conjure-${CONJURE_VERSION}/bin/conjure compile demo.yml demo.conjure.json &&
-conjure-typescript generate --rawSource demo.conjure.json conjure-api
+conjure-typescript generate --rawSource demo.conjure.json conjure-api &&
+rm demo.conjure.json

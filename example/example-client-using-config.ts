f2f4e09113a650e8ac88cd28ab8f44437f8c585b
Yury Namgung <ynamgung@palantir.com>
2020-10-08 12:44:38 -0400

    delete misc files

--- example/example-client-using-config.ts
+++
@@ -1,7 +0,0 @@
-import { DefaultHttpApiBridge, FirebaseApiBridge } from "conjure-firebase";
-import { FirebaseFunctionsService as __FirebaseFunctionsService } from "./conjure-api";
-
-const fab = new FirebaseApiBridge();
-const FirebaseFunctionsService = new __FirebaseFunctionsService(fab);
-
-export { FirebaseFunctionsService };
